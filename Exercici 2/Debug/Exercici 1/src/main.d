5e55980fdb92aa3143e335757e448de616a394bb
Nil Carmona Quesada <95416766+Nilotis21@users.noreply.github.com>
2022-01-19 22:32:18 +0100

    Delete Exercici 2 directory

--- Exercici 2/Debug/Exercici 1/src/main.d
+++
@@ -1,1 +0,0 @@
-Exercici 1/src/main.o: ../Exercici\ 1/src/main.c
--- Exercici 2/src/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H_
-#define FUNCTIONS_H_
-
-float validar();
-float dias(float);
-float horas(float);
-float minutos(float);
-
-#endif /* FUNCTIONS_H_ */
