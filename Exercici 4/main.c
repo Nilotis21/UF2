37f9de151a42fe167fe1fb064dfffd5df72a0f17
Nil Carmona Quesada <95416766+Nilotis21@users.noreply.github.com>
2022-01-20 20:04:20 +0100

    Update main.c

--- Exercici 4/main.c
+++ Exercici 4/main.c
@@ -1,24 +1,17 @@
-#include <stdio.h>
-#include "functions.h"
-
-#define SIZE 15
-
 void main() {
 
-	int num, i, ap = 0, sus = 0, avg_ap = 0, avg_su = 0;
+	int num, i, ap = 0, sus = 0;
+  float avg_ap = 0, avg_su = 0;
 
 	for (i = 0; i < SIZE; i++) {
 		num = validar();
 
-		sus = suspensos(num) + sus;
-
-		ap = aprovados(num) + ap;
-
-		avg_ap = avgap(num) + avg_ap;
-
-		avg_su = avgsu(num) + avg_su;
+		sus = calc_not(num) + sus;
+    ap = SIZE - sus;
+		avg_ap = avg_note(num, ap);
+		avg_su = avg_note(num, sus);
 	}
-	printf("La quantitat d'aprovats és de %d i la de suspesos és %d.\n\n", ap, sus);
-	printf("La mitjana d'aprovats és de %d i la de suspesos és %d.", avg_ap/ ap, avg_su / sus);
+  printf("La mitjana d'aprovats és de %.2f i la de suspesos és %.2f.\n", avg_ap/ ap, avg_su / sus);
+	printf("La quantitat d'aprovats és de %d i la de suspesos és %d.\n", ap, sus);
 
 }
