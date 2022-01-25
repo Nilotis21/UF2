43d8fb8fa904fcf6e888a5edae9bed3ad34061f1
Nil Carmona Quesada <95416766+Nilotis21@users.noreply.github.com>
2022-01-25 10:53:39 -0800

    Exercici 7 pujat.
    Exercici 7 pujat.

---
+++ Exercici 7/functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#define MSG "Quants registres introduïras:\n"
+#define MSG2 "Introdueix un nombre[1-10]:\n"
+
+int validar();
+int validar2();
+void omplir(int *, int);
+int mitjana(int *, int);
+int max(int *, int);
+int min(int *, int);
+
+
+#endif /* FUNCTIONS_H_ */
