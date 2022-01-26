5ad04af214d7c2d31eda65bf7fb91e89b0e1bfd2
Nil Carmona Quesada <95416766+Nilotis21@users.noreply.github.com>
2022-01-26 05:26:35 -0800

    Correció de calcul de la mitjana.
    Correció de calcul de la mitjana.

---
+++ Exercici 7/functions.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "functions.h"
+
+float validar(char *msg){
+
+	float num;
+	do{
+		printf("%s", msg);
+		scanf("%f", &num);
+	} while (num < 1 || num > 50);
+
+	return num;
+}
+
+float validar2(char *msg){
+
+	float num;
+	do{
+		printf("%s", msg);
+		scanf("%f", &num);
+	} while (num < 1 || num > 10);
+
+	return num;
+}
+
+void omplir(float *num, float size){
+
+	int i;
+
+	for (i = 0; i < size; i++){
+		num[i] = validar2(MSG2);
+	}
+}
+
+float mitjana(float *num, float size){
+
+	int i;
+	float j = 0;
+
+	for (i = 0; i < size; i++){
+		j += num[i];
+	}
+	return j / size;
+}
+
+float max(float *num, float size){
+
+	int i;
+	float j = 0;
+
+	for (i = 0; i < size; i++){
+		if(num[i] > j){
+			j = num[i];
+		}
+	}
+	return j;
+}
+
+float min(float *num, float size){
+
+	int i = 0;
+	float j = 1;
+
+	j = num[i];
+	for (i = 0; i < size; i++){
+		if(j > num[i]){
+			j = num[i];
+		}
+	}
+	return j;
+}
---
+++ Exercici 7/functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#define MSG "Quants registres introduïras:\n"
+#define MSG2 "Introdueix un nombre[1-10]:\n"
+
+float validar();
+float validar2();
+void omplir(float *, float);
+float mitjana(float *, float);
+float max(float *, float);
+float min(float *, float);
+
+
+#endif /* FUNCTIONS_H_ */
