9f9f2509e95ef467d39eef68a97a7356ba55c838
Nil Carmona Quesada <95416766+Nilotis21@users.noreply.github.com>
2022-02-02 14:12:26 +0100

    Exercici acabat amb l'ordenacio implementada.
    Exercici acabat amb l'ordenacio implementada.

---
+++ Exercici 7/functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+#define MSG "Quants registres introduïras:\n"
+#define MSG2 "Introdueix un nombre[1-10]:\n"
+
+float validar(char *, int, int);
+void omplir(float *, int);
+float mitjana(float *, int);
+float max(float *, int);
+float min(float *, int);
+void order(float *, int);
+
+
+#endif /* FUNCTIONS_H_ */
---
+++ Exercici 7/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "functions.h"
+
+void main() {
+
+	int size, i;
+	size = validar(MSG, 1, 50);
+	float num[size];
+	omplir(num, size);
+	printf("\nLa mitjana dels valors és: %.2f\n"
+			"El valor més gran és %.0f i el "
+			"més petit és %.0f.\n", mitjana(num, size), max(num, size),
+			min(num, size));
+
+	order(num, size);
+
+	printf("Els valors ordenats són: [");
+	for (i = 0; i < size; i++) {
+		if (i != size-1) {
+			printf("%.0f,", num[i]);
+		} else {
+			printf("%.0f", num[i]);
+		}
+	}
+	printf("]");
+}
