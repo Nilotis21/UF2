ee6a5c765d6c2153271c790b329b50c8cb5bb7d6
Nil Carmona Quesada <95416766+Nilotis21@users.noreply.github.com>
2022-02-08 20:48:09 +0100

    Exercici 13 pujat.
    Exercici 13 pujat.

---
+++ Exercici_13/functions.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include "functions.h"
+
+int valint(char *msg, int ini, int fin) {
+
+	int number;
+
+	if (fin == NULL) {
+
+		do {
+			printf(msg);
+			scanf("%d", &number);
+		} while (number < ini);
+
+	} else if (ini == NULL) {
+
+		do {
+			printf(msg);
+			scanf("%d", &number);
+		} while (number > fin);
+
+	} else if (ini != NULL && fin != NULL) {
+
+		do {
+			printf(msg);
+			scanf("%d", &number);
+		} while (number < ini || number > fin);
+
+	} else {
+
+		printf(msg);
+		scanf("%d", &number);
+
+	}
+	return number;
+}
+
+void omplir(int *number, int size, int ini, int fin) {
+
+	for (int i = ini; (i < size) && (i < fin); i++) {
+		number[i] = i;
+	}
+}
+
+void order_des(int *number, int size) {
+
+	int i, j, aux;
+
+	for (i = 0; i < size; i++) {
+		for (j = i + 1; j < size; j++) {
+			if (number[j] > number[i]) {
+				aux = number[i];
+				number[i] = number[j];
+				number[j] = aux;
+			}
+
+		}
+	}
+}
+
+void order_asc(int *number, int size) {
+
+	int i, j, aux;
+
+	for (i = 0; i < size; i++) {
+		for (j = i + 1; j < size; j++) {
+			if (number[j] < number[i]) {
+				aux = number[i];
+				number[i] = number[j];
+				number[j] = aux;
+			}
+
+		}
+	}
+}
+
+int cercar(int *number, int size, int aux2) {
+
+	int i, trobat = 0;
+
+	for (i = 0; (i < size) && (trobat == 0); i++) {
+		if (number[i] == aux2) {
+			trobat = 1;
+		}
+	}
+	return trobat;
+}
